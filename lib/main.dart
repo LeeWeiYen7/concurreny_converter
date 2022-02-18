622c9017ec9656097f3abc5f5954f56dbf4cf2c4
LeeWeiYen7 <godwind721@gmail.com>
2022-02-18 23:59:20 +0800

    Update

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'converter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currency Converter',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({ Key? key }) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+         
+          
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(context, 
+      MaterialPageRoute(builder: (content) => const ConversionPage()))
+    );
+  }
+}
